7437a7bef7b4755155e5660da29d4d134554d16d
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-01-21 13:19:03 +0700

    feat: add script to bump version

--- .releaserc.js
+++ .releaserc.js
@@ -8,6 +8,10 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/npm",
-        "@semantic-release/github"
+        "@semantic-release/github",
+        ["@semantic-release/exec", {
+            // "prepareCmd": "echo -n ${nextRelease.version} > VERSION",
+            "prepareCmd": "node ./buildScripts/bumpVersion.js ${nextRelease.version}",
+        }]
     ]
 }
