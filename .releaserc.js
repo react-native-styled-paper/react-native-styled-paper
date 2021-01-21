76ad89d50fffb7e53ee6f93556e54d936f6a9c99
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-01-21 10:19:45 +0700

    feat: add semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        // "@semantic-release/npm",
-        // "@semantic-release/gitlab"
+        "@semantic-release/npm",
+        "@semantic-release/github"
     ]
 }
