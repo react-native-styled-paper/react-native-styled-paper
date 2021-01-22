522037fcebed177ca9788fd0aa64e3c387257161
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-01-22 16:53:13 +0700

    fix: update storybook

--- examples/expo-example/storybook/index.js
+++ examples/expo-example/storybook/index.js
@@ -1,17 +1,22 @@
-// if you use expo remove this line
+import React from 'react';
 import { AppRegistry } from 'react-native';
-
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
-import { withKnobs } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'styled-components';
+import { LightTheme } from 'react-native-styled-paper';
+import { loadStories } from './storyLoader';
 
 import './rn-addons';
 
-// enables knobs for all stories
-addDecorator(withKnobs);
-
-// import stories
 configure(() => {
-  require('./stories');
+    addDecorator((storyFn) => (
+        // <LocaleProvider locale={'EN-gb'}>
+            <ThemeProvider theme={LightTheme}>
+                {storyFn()}
+            </ThemeProvider>
+        // </LocaleProvider>
+    ));
+
+    loadStories()
 }, module);
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
@@ -19,7 +24,7 @@ configure(() => {
 const StorybookUIRoot = getStorybookUI({});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+// If you use Expo you can safely remove this line.
+AppRegistry.registerComponent('mobile', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
