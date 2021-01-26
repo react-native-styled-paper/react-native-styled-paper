e9e837a2638b487684b5620c3a769eadba4a3978
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-01-26 21:55:51 +0700

    fix: add more knobs

--- examples/cra-example/components/Button/Button.stories.js
+++ examples/cra-example/components/Button/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { Button } from 'react-native-styled-paper';
 
@@ -17,4 +17,19 @@ storiesOf('Button', module)
             {title}
         </Button>
         )
-    });
+    })
+    .add('Primary', () => {
+        const title = text("title", "Text");
+        const mode = select('theme', ['text', 'outlined', 'contained'], "text");
+        const dark = boolean('dark', false);
+
+        return (
+        <Button
+            dark={dark}
+            mode={mode}
+            onPress={onPressFn}
+        >
+            {title}
+        </Button>
+        )
+    })
