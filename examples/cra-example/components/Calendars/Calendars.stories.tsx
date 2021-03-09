8471b9cba41b0f179c8965f5d1d6cd53f4aff2d2
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-03-09 11:13:13 +0700

    fix(calendars): add more examples

--- examples/cra-example/components/Calendars/Calendars.stories.tsx
+++ examples/cra-example/components/Calendars/Calendars.stories.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import AgendaExample from "./agenda";
 import CalendarsExample from "./calendars";
 import CalendarsListExample from "./calendarsList";
 
 storiesOf("Calendars", module)
+    .add("Agenda", () => {
+
+        return (
+            <AgendaExample
+            />
+        )
+    })
     .add("Calendars", () => {
 
         return (
